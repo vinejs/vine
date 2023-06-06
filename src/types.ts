b870bdcde60830ee27e8ff78ad77907c48c0f463
Harminder Virk <virk.officials@gmail.com>
2023-06-06 15:15:00 +0530

    feat: implement initial types

--- src/types.ts
+++ src/types.ts
@@ -13,6 +13,7 @@ import type {
   TransformFn,
   FieldContext,
   CompilerNodes,
+  MessagesProviderContact,
   ErrorReporterContract as BaseReporter,
 } from '@vinejs/compiler/types'
 import type { ValidationError } from './errors/validation_error.js'
@@ -21,19 +22,37 @@ import type { BRAND, CBRAND, PARSE, VALIDATION } from './symbols.js'
 /**
  * Re-exporting selected types from compiler
  */
-export type { Refs, RefIdentifier, FieldContext, ConditionalFn } from '@vinejs/compiler/types'
-
-/**
- * Error reporters must implement the reporter contract interface
- */
-export interface ErrorReporterContract extends BaseReporter {
-  createError(): ValidationError
-}
+export type {
+  Refs,
+  FieldContext,
+  RefIdentifier,
+  ConditionalFn,
+  MessagesProviderContact,
+} from '@vinejs/compiler/types'
 
 /**
  * Representation of a native enum like type
  */
 export type EnumLike = { [K: string]: string | number; [number: number]: string }
+
+/**
+ * Representation of fields and messages accepted by the messages
+ * provider
+ */
+export type ValidationMessages = Record<string, string>
+export type ValidationFields = Record<string, string>
+
+/**
+ * Constructable schema type refers to any type that can be
+ * constructed for type inference and compiler output
+ */
+export interface ConstructableSchema<Output, CamelCaseOutput> {
+  [BRAND]: Output
+  [CBRAND]: CamelCaseOutput
+  [PARSE](propertyName: string, refs: RefsStore, options: ParserOptions): CompilerNodes
+  clone(): this
+}
+export type SchemaTypes = ConstructableSchema<any, any>
 
 /**
  * Representation of a function that performs validation.
@@ -124,39 +143,15 @@ export type ParserOptions = {
 export type UnionNoMatchCallback<Input> = (value: Input, ctx: FieldContext) => any
 
 /**
- * Messages provider is used to resolve validation error messages
- * during validation.
+ * Error reporters must implement the reporter contract interface
  */
-export interface MessagesProviderContact {
-  /**
-   * Returns a validation message for a given field + rule. The args
-   * may get defined by a validation rule to share additional context.
-   */
-  getMessage(
-    rawMessage: string,
-    rule: string,
-    ctx: FieldContext,
-    args?: Record<string, any>
-  ): string
+export interface ErrorReporterContract extends BaseReporter {
+  createError(): ValidationError
 }
 
 /**
- * Constructable schema type refers to any type that can be
- * constructed for type inference and compiler output
- */
-export interface ConstructableSchema<Output, CamelCaseOutput> {
-  [BRAND]: Output
-  [CBRAND]: CamelCaseOutput
-  [PARSE](propertyName: string, refs: RefsStore, options: ParserOptions): CompilerNodes
-}
-
-/**
- * Possible schema types. This list is as broad as it can be
- */
-export type SchemaTypes = ConstructableSchema<any, any>
-
-/**
- * Options accepted by vine
+ * Options accepted by vine.configure and the validate
+ * method.
  */
 export type VineOptions = {
   /**
@@ -168,36 +163,28 @@ export type VineOptions = {
    * Messages provider is used to resolve error messages during
    * the validation lifecycle
    */
-  messagesProvider: (messages: Record<string, string>) => MessagesProviderContact
+  messagesProvider: (
+    messages: ValidationMessages,
+    fields: ValidationFields
+  ) => MessagesProviderContact
 
   /**
    * Validation errors are reported directly to an error reporter. The reporter
    * can decide how to format and output errors.
    */
-  errorReporter: (messagesProvider: MessagesProviderContact) => ErrorReporterContract
+  errorReporter: () => ErrorReporterContract
+}
+
+/**
+ * Validation options accepted by the validate method
+ */
+export type ValidationOptions = {
+  data: any
+  messages?: ValidationMessages
+  fields?: ValidationFields
 }
 
 /**
  * Infers the schema type
  */
 export type Infer<Schema extends ConstructableSchema<any, any>> = Schema[typeof BRAND]
-
-/**
- * Validation options accepted by the validate method
- */
-export type ValidationOptions =
-  | {
-      data: any
-    }
-  | {
-      data: any
-      messages: Record<string, any>
-    }
-  | {
-      data: any
-      messagesProvider: MessagesProviderContact
-    }
-  | {
-      data: any
-      errorReporter: ErrorReporterContract
-    }
