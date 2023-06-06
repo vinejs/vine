77ca2d723e9c9b23e0265961d14fb255e28f9e8f
Harminder Virk <virk.officials@gmail.com>
2023-06-06 10:09:28 +0530

    feat: finalize default error messages

---
+++ src/defaults.ts
@@ -0,0 +1,67 @@
+/*
+ * @vinejs/vine
+ *
+ * (c) VineJS
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
+ * Collection of default error messages used by VineJS core
+ */
+export const errorMessages = {
+  'required': 'The {{ field }} field must be defined',
+
+  'string': 'The {{ field }} field must be a string',
+  'email': 'The {{ field }} field must be a valid email address',
+  'regex': 'The {{ field }} field format is invalid',
+  'url': 'The {{ field }} field must be a valid URL',
+  'activeUrl': 'The {{ field }} field must be a valid URL',
+  'alpha': 'The {{ field }} field must contain only letters',
+  'alphaNumeric': 'The {{ field }} field must contain only letters and numbers',
+  'minLength': 'The {{ field }} field must have at least {{ min }} characters',
+  'maxLength': 'The {{ field }} field must not be greater than {{ max }} characters',
+  'fixedLength': 'The {{ field }} field must be {{ size }} characters long',
+  'confirmed': 'The {{ field }} field and {{ otherField }} field must be the same',
+  'endsWith': 'The {{ field }} field must end with {{ substring }}',
+  'startsWith': 'The {{ field }} field must start with {{ substring }}',
+  'sameAs': 'The {{ field }} field and {{ otherField }} field must be the same',
+  'notSameAs': 'The {{ field }} field and {{ otherField }} field must be different',
+  'in': 'The selected {{ field }} is invalid',
+  'notIn': 'The selected {{ field }} is invalid',
+  'ipAddress': 'The {{ field }} field must be a valid IP address',
+  'uuid': 'The {{ field }} field must be a valid UUID',
+
+  'boolean': 'The value must be a boolean',
+
+  'number': 'The {{ field }} field must be a number',
+  'min': 'The {{ field }} field must be at least {{ min }}',
+  'max': 'The {{ field }} field must not be greater than {{ max }}',
+  'range': 'The {{ field }} field must be between {{ min }} and {{ max }}',
+  'positive': 'The {{ field }} field must be positive',
+  'negative': 'The {{ field }} field must be negative',
+  'decimal': 'The {{ field }} field must have {{ value }} decimal places',
+
+  'accepted': 'The {{ field }} field must be accepted',
+
+  'enum': 'The selected {{ field }} is invalid',
+
+  'literal': 'The {{ field }} field must be {{ expectedValue }}',
+
+  'object': 'The {{ field }} field must be an object',
+
+  'array': 'The {{ field }} field must be an array',
+  'array.minLength': 'The {{ field }} field must have at least {{ min }} items',
+  'array.maxLength': 'The {{ field }} field must not have more than {{ max }} items',
+  'array.fixedLength': 'The {{ field }} field must contain {{ size }} items',
+  'notEmpty': 'The {{ field }} field must not be empty',
+  'distinct': 'The {{ field }} field has duplicate values',
+
+  'record': 'The {{ field }} field must be an object',
+  'record.minLength': 'The {{ field }} field must have at least {{ min }} items',
+  'record.maxLength': 'The {{ field }} field must not have more than {{ max }} items',
+  'record.fixedLength': 'The {{ field }} field must contain {{ size }} items',
+
+  'tuple': 'The {{ field }} field must be an array',
+}
