fde2e25bb64997a2b1f0dd5a7af1f22f288f6604
Harminder Virk <virk.officials@gmail.com>
2025-10-14 14:33:35 +0530

    style: remove unused imports

--- src/vine/helpers.ts
+++ src/vine/helpers.ts
@@ -31,7 +31,6 @@ import { locales as mobilePhoneLocales } from 'validator/lib/isMobilePhone.js'
 import { locales as postalCodeLocales } from 'validator/lib/isPostalCode.js'
 
 import type { FieldContext } from '../types.js'
-import { FieldNode } from '@vinejs/compiler/types'
 
 const BOOLEAN_POSITIVES = ['1', 1, 'true', true, 'on']
 const BOOLEAN_NEGATIVES = ['0', 0, 'false', false]
