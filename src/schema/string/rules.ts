5617a5098ef4ef0a774acf72a79ab4f15e9ab2a5
Romain Lanz <2793951+RomainLanz@users.noreply.github.com>
2026-09-18 08:56:30 +0200

    perf: avoid rebuilding alphaNumeric regex on every validation (#154)

--- src/schema/string/rules.ts
+++ src/schema/string/rules.ts
@@ -233,6 +233,19 @@ export function alphaRule(options?: AlphaOptions) {
   return alphaValidator(new RegExp(`^[${characterSet}]+$`))
 }
 
+const alphaNumericValidator = createRule<RegExp>(
+  function alphaNumeric(value, expression, field) {
+    if (!expression.test(value as string)) {
+      field.report(messages.alphaNumeric, 'alphaNumeric', field)
+    }
+  },
+  {
+    toJSONSchema: (schema, expression) => {
+      schema.pattern = expression.source
+    },
+  }
+)
+
 /**
  * Validates the value to contain only alphanumeric characters (letters and numbers).
  * Supports options to allow spaces, dashes, and underscores.
@@ -242,45 +255,20 @@ export function alphaRule(options?: AlphaOptions) {
  * vine.string().alphaNumeric({ allowSpaces: true })
  * vine.string().alphaNumeric({ allowDashes: true, allowUnderscores: true })
  */
-export const alphaNumericRule = createRule<AlphaNumericOptions | undefined>(
-  function alphaNumeric(value, options, field) {
-    let characterSet = 'a-zA-Z0-9'
-    if (options) {
-      if (options.allowSpaces) {
-        characterSet += '\\s'
-      }
-      if (options.allowDashes) {
-        characterSet += '-'
-      }
-      if (options.allowUnderscores) {
-        characterSet += '_'
-      }
-    }
-
-    const expression = new RegExp(`^[${characterSet}]+$`)
-    if (!expression.test(value as string)) {
-      field.report(messages.alphaNumeric, 'alphaNumeric', field)
-    }
-  },
-  {
-    toJSONSchema: (schema, options) => {
-      let characterSet = 'a-zA-Z0-9'
-      if (options) {
-        if (options.allowSpaces) {
-          characterSet += '\\s'
-        }
-        if (options.allowDashes) {
-          characterSet += '-'
-        }
-        if (options.allowUnderscores) {
-          characterSet += '_'
-        }
-      }
-
-      schema.pattern = `^[${characterSet}]+$`
-    },
+export function alphaNumericRule(options?: AlphaNumericOptions) {
+  let characterSet = 'a-zA-Z0-9'
+  if (options?.allowSpaces) {
+    characterSet += '\\s'
   }
-)
+  if (options?.allowDashes) {
+    characterSet += '-'
+  }
+  if (options?.allowUnderscores) {
+    characterSet += '_'
+  }
+
+  return alphaNumericValidator(new RegExp(`^[${characterSet}]+$`))
+}
 
 /**
  * Enforces a minimum length on a string field.
